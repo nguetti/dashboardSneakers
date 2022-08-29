1f429b0c8c978054b309117874208d0a04997658
stornelli23 <santystornelli@hotmail.com>
2022-08-29 20:59:59 -0300

    funcinando totalsrow

--- dashboard_sneakers/src/components/TotalsRow/TotalUsers.js
+++ dashboard_sneakers/src/components/TotalsRow/TotalUsers.js
@@ -1,11 +1,33 @@
 import './TotalsRow.css'
+import { useEffect, useState } from "react";
 
 
 function TotalUsers () {
+
+    const [totalUsers, setTotalUsers] = useState([]);
+    useEffect(() => {
+    //   console.log("%cse monto el componente", "color: green");
+      fetch("/api/users")
+        .then((result) => result.json())
+        .then((data) => {
+          setTotalUsers(data.total);
+        });
+    }, []);
+    
+    useEffect(() => {
+    //   console.log("%cse actualizo el componente", "color: blue");
+    }, [totalUsers]);
+    
+    useEffect(() => {
+      return () => console.log("%cse desmonto el componente", "color: red");
+    });
+
+
+
     return (
 <div className="totalsBox" >
     <p className="title">Total de usuarios</p>
-    <p className="total">50</p>
+    <p className="total">{totalUsers}</p>
 </div>
     )
 }
